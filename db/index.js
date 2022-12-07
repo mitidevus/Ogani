062ef36cfe888684ba83e731b36b6471e73cd41c
Đỗ Minh Trí <90955782+mitidevus@users.noreply.github.com>
2022-12-07 16:17:22 +0700

    Merge fix_list_products to dev

--- db/index.js
+++ db/index.js
@@ -3,13 +3,15 @@ const mysql = require('mysql2/promise');
 
 const db = { connection: null };
 
+require("dotenv").config();
+
 (async () => {
   // create the connection to database
   db.connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'ogani',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
   console.log('Database connected!');
 })();
