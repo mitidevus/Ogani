f21dda361487252b3ab97b4f7598e0e9c7712903
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2022-12-14 16:35:14 +0700

    update

--- db/index.js
+++ db/index.js
@@ -8,14 +8,10 @@ const db = { connection: null };
 (async () => {
   // create the connection to database
   db.connection = await mysql.createConnection({
-    // host: "us-cdbr-east-06.cleardb.net",
-    // user: "b0970366aef1fb",
-    // password: "ed5e0c83",
-    // database:"heroku_4932217c006f01d"
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'ogani'
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b0970366aef1fb",
+    password: "ed5e0c83",
+    database: "heroku_4932217c006f01d"
   });
   console.log('Database connected!');
 })();
