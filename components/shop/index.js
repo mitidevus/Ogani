6daf031cf8b40cfae84e71ca3003118759820a73
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2022-12-19 11:35:23 +0700

    Update sort when filter by category

--- components/shop/index.js
+++ components/shop/index.js
@@ -3,20 +3,12 @@ const router = express.Router();
 
 const shopController = require('./shopController');
 
-// router.get('/', shopController.shop);
-// router.get('/sort/:filter', shopController.shop);
-// router.get('/product_category/:category', shopController.category);
-
 router.get('/page/:page', shopController.shop);
 
 router.get('/page/:page/sort/:filter', shopController.shop);
 
 router.get('/product_category/:category/page/:page', shopController.category);
-// router.get('/product_category/', shopController.category);
 
-// router.get('/sort/:filter/page/:numPage', shopController.shop);
-
-
-// router.get('/product_category/:category/page/:numPage', shopController.category);
+router.get('/product_category/:category/page/:page/sort/:filter', shopController.category);
 
 module.exports = router;
--- components/shop/shopController.js
+++ components/shop/shopController.js
@@ -1,54 +1,6 @@
 const shopService = require('./shopService');
 const qs = require('qs');
-const itemsPerPage = 2;
-
-// exports.shop = async (req, res) => {
-//   const filter = req.params.filter;
-//   const { name: nameFilter } = req.query;
-
-//   let listProducts = [];
-//   if (nameFilter) {
-//     listProducts = await shopService.filter(nameFilter);
-//   }
-//   else if (filter === "price-asc") {
-//     listProducts = await shopService.getSortedProductByPrice_ASC();
-//   }
-//   else if (filter === "price-desc") {
-//     listProducts = await shopService.getSortedProductByPrice_DESC();
-//   }
-//   else if (filter === "date-new") {
-//     listProducts = await shopService.getSortedProductByRelease_Date_Latest();
-//   }
-//   else if (filter === "date-old") {
-//     listProducts = await shopService.getSortedProductByRelease_Date_Oldest();
-//   }
-//   else if (filter === "rate-star-asc") {
-//     listProducts = await shopService.getSortedProductByRate_Star_ASC();
-//   }
-//   else if (filter === "rate-star-desc") {
-//     listProducts = await shopService.getSortedProductByRate_Star_DESC();
-//   }
-//   else {
-//     listProducts = await shopService.getAllProduct();
-//   }
-
-//   console.log("listProducts", listProducts);
-
-//   for (let i = 0; i < listProducts.length; i++) {
-//     listProducts[i].price = listProducts[i].price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
-//   }
-
-//   let listCategory = await shopService.getAllCategory();
-
-//   let latestProduct = await shopService.getSortedProductByRelease_Date_Latest();
-//   latestProduct = latestProduct.slice(0, 5);
-
-//   for (let i = 0; i < latestProduct.length; i++) {
-//     latestProduct[i].price = latestProduct[i].price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
-//   }
-
-//   res.render('shop/page', { listProducts, listCategory, latestProduct, originalUrl: `${req.baseUrl}/${qs.stringify(filter)}` });
-// };
+const itemsPerPage = 3;
 
 exports.shop = async (req, res) => {
   const filter = req.params.filter;
@@ -68,11 +20,14 @@ exports.shop = async (req, res) => {
   if (filter) url_sort = "/sort/" + filter;
 
   let listProducts = [];
+  if (!nameFilter && !filter) {
+    listProducts = await shopService.getAllProduct();
+  }
   if (nameFilter) {
     listProducts = await shopService.filter(nameFilter);
     console.log("listProducts Filter", listProducts);
   }
-  else if (filter === "price-asc") {
+  if (filter === "price-asc") {
     if (listProducts.length === 0)
       listProducts = await shopService.getSortedProductByPrice_ASC();
     else listProducts.sort((a, b) => a.price - b.price);
@@ -83,16 +38,6 @@ exports.shop = async (req, res) => {
       listProducts = await shopService.getSortedProductByPrice_DESC();
     else listProducts.sort((a, b) => b.price - a.price);
   }
-  // else if (filter === "date-new") {
-  //   if (!nameFilter)
-  //     listProducts = await shopService.getSortedProductByRelease_Date_Latest();
-  //   else listProducts.sort((a, b) => a.release_date - b.release_date);
-  // }
-  // else if (filter === "date-old") {
-  //   if (!nameFilter)
-  //     listProducts = await shopService.getSortedProductByRelease_Date_Oldest();
-  //   else listProducts.sort((a, b) => b.release_date - a.release_date);
-  // }
   else if (filter === "rate-star-asc") {
     if (listProducts.length === 0)
       listProducts = await shopService.getSortedProductByRate_Star_ASC();
@@ -103,12 +48,10 @@ exports.shop = async (req, res) => {
       listProducts = await shopService.getSortedProductByRate_Star_DESC();
     else listProducts.sort((a, b) => b.rate_star - a.rate_star);
   }
-  else {
-    listProducts = await shopService.getAllProduct();
-  }
+
   console.log("listProducts.length", listProducts.length);
 
-  const sumPage = Math.ceil(listProducts.length / itemsPerPage);//tổng số page cần có để chứa các sp
+  const sumPage = Math.ceil(listProducts.length / itemsPerPage); //tổng số page cần có để chứa các sp
 
   let listTemp = [];
   for (let i = (currentPage - 1) * itemsPerPage; i < (currentPage - 1) * itemsPerPage + itemsPerPage; i++)
@@ -118,7 +61,6 @@ exports.shop = async (req, res) => {
       console.log("listProducts[i]", listProducts[i]);
     }
   listProducts = listTemp;
-
 
   for (let i = 0; i < listProducts.length; i++) {
     listProducts[i].price = listProducts[i].price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
@@ -151,60 +93,37 @@ exports.shop = async (req, res) => {
   res.render('shop/page', { url_sort, url_filter, listProducts, listCategory, listLeftPage, listcurrentPage, listRightPage, latestProduct, originalUrl: `/shop/page/1/${qs.stringify(filter)}` });
 };
 
-// exports.category = async (req, res) => {
-//   let cate_Id = req.params.category;
-//   const { name: nameFilter } = req.query;
-//   console.log("nameFilter", nameFilter);
-
-//   let listProducts = [];
-
-//   if (nameFilter) {
-//     let allProductByCategory = await shopService.getProductByCategory(cate_Id);
-//     allProductByCategory.forEach((product) => {
-//       if (product.name.toLowerCase().includes(nameFilter.toLowerCase())) {
-//         listProducts.push(product);
-//       }
-//     });
-//   }
-//   else {
-//     listProducts = await shopService.getProductByCategory(cate_Id);
-//   }
-
-//   for (let i = 0; i < listProducts.length; i++) {
-//     listProducts[i].price = listProducts[i].price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
-//   }
-
-//   let listCategory = await shopService.getAllCategory();
-
-//   let latestProduct = await shopService.getSortedProductByRelease_Date_Latest();
-//   latestProduct = latestProduct.slice(0, 5);
-//   for (let i = 0; i < latestProduct.length; i++) {
-//     latestProduct[i].price = latestProduct[i].price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
-//   }
-
-//   res.render('shop/page', { listProducts, latestProduct, listCategory, originalUrl: `${req.baseUrl}/${qs.stringify(cate_Id)}` });
-// }
-
 exports.category = async (req, res) => {
   let cate_Id = req.params.category;
-  const { name: nameFilter } = req.query;
-  // const { sort: nameFilter } = req.params.;
+  const filter = req.params.filter; // sort
   let currentPage = req.params.page;
   const checkCategory = true;
   let url_link = "/product_category/" + cate_Id;
+  let url_sort = "";
 
   console.log("currentPage", currentPage);
-  console.log("nameFilter", nameFilter);
+
+  if (filter) url_sort = "/sort/" + filter;
 
   let listProducts = [];
 
-  if (nameFilter) {
-    let allProductByCategory = await shopService.getProductByCategory(cate_Id);
-    allProductByCategory.forEach((product) => {
-      if (product.name.toLowerCase().includes(nameFilter.toLowerCase())) {
-        listProducts.push(product);
-      }
-    });
+  if (filter === "price-asc") {
+    listProducts = await shopService.getProductByCategory(cate_Id);
+    listProducts.sort((a, b) => a.price - b.price);
+  }
+  else if (filter === "price-desc") {
+    listProducts = await shopService.getProductByCategory(cate_Id);
+    listProducts.sort((a, b) => b.price - a.price);
+  }
+  else if (filter === "rate-star-asc") {
+    listProducts = await shopService.getProductByCategory(cate_Id);
+    listProducts.sort((a, b) => a.rate_Star - b.rate_Star);
+    console.log("listProducts rate_Start up", listProducts);
+  }
+  else if (filter === "rate-star-desc") {
+    listProducts = await shopService.getProductByCategory(cate_Id);
+    listProducts.sort((a, b) => b.rate_Star - a.rate_Star);
+    console.log("listProducts rate_Star down", listProducts);
   }
   else {
     listProducts = await shopService.getProductByCategory(cate_Id);
@@ -247,5 +166,5 @@ exports.category = async (req, res) => {
 
   console.log("url_link", url_link);
 
-  res.render('shop/page', { url_link, cate_Id, checkCategory, listProducts, latestProduct, listCategory, listLeftPage, listcurrentPage, listRightPage, originalUrl: `${req.baseUrl}/${qs.stringify(cate_Id)}` });
+  res.render('shop/page', { url_sort, url_link, cate_Id, checkCategory, listProducts, latestProduct, listCategory, listLeftPage, listcurrentPage, listRightPage, originalUrl: `/shop/product_category/${cate_Id}/page/1/${qs.stringify(filter)}` });
 }
